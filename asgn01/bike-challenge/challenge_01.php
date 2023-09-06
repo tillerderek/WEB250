45d5a03214a5709805dd4a9645bd1874a6b2a358
derek <tillerderek@gmail.com>
2023-09-05 20:18:39 -0400

    formatted bikes

--- asgn01/bike-challenge/challenge_01.php
+++ asgn01/bike-challenge/challenge_01.php
@@ -1,6 +1,7 @@
 <?php
 
-class Bicycle {
+class Bicycle
+{
 
   var $brand;
   var $model;
@@ -8,19 +9,23 @@ class Bicycle {
   var $description;
   var $weight_kg;
 
-  function name() {
+  function name()
+  {
     return $this->brand . " " . $this->model . " (" . $this->year . ")";
   }
 
-  function weight_lbs() {
+  function weight_lbs()
+  {
     return round(floatval($this->weight_kg) * 2.2046226218) . " lbs";
   }
 
-  function set_weight_lbs($value) {
+  function set_weight_lbs($value)
+  {
     $this->weight_kg = round(floatval($value) / 2.2046226218) . " lbs";
   }
 
-  function weight_kg() {
+  function weight_kg()
+  {
     return $this->weight_kg . " kg";
   }
 
@@ -39,13 +44,17 @@ $jamis->year = '2017';
 $jamis->weight_kg = 12.0;
 
 echo $salsa->name() . "<br />";
-echo $jamis->name() . "<br />";
 echo $salsa->weight_kg() . "<br />";
 echo $salsa->weight_lbs() . "<br />";
+echo $jamis->name() . "<br />";
 echo $jamis->weight_kg() . "<br />";
 echo $jamis->weight_lbs() . "<br />";
 $salsa->set_weight_lbs(20);
-$jamis->set_weight_lbs(25); 
-echo $salsa->weight_kg . "<br />";
-echo $jamis->weight_kg . "<br />";
+$jamis->set_weight_lbs(25);
+echo "revised weights: <br />";
+echo $salsa->name() . "<br />";
+echo $salsa->weight_lbs() . "<br />";
+echo $jamis->name() . "<br />";
+echo $jamis->weight_lbs() . "<br />";
+
 ?>
