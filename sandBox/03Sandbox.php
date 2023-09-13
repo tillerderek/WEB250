5985c2ea081af04ddd2f222a73d4c8054a5eb051
derek <tillerderek@gmail.com>
2023-09-13 13:52:46 -0400

    notes

--- sandBox/03Sandbox.php
+++ sandBox/03Sandbox.php
@@ -11,3 +11,8 @@ static basically can only call from class and its contents can't make new instan
 
 basically a class that is just methods, could use it inherated in a new class or 
 for something like db_connect or whatever to just run something. 
+
+
+use self:: when calling og class methods
+use static:: when calling inherated sublcasses, otherwise self will just call the methods out of the original class due to it being pre-procsessed. 
+
