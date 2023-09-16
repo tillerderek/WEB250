96a8a97c53991d980771bd7097607c4874f68c21
derek <tillerderek@gmail.com>
2023-09-15 23:27:25 -0400

    dev branch done

--- asgn03-static/Bird.php
+++ asgn03-static/Bird.php
@@ -1,31 +1,43 @@
 <?php
 
-class Bird {
-    var $habitat;
-    var $food;
-    var $nesting = "tree";
-    var $conservation;
-    var $song = "chirp";
-    var $flying = "yes";
+class Bird
+{
+  var $habitat;
+  var $food;
+  var $nesting = "tree";
+  var $conservation;
+  var $song = "chirp";
+  public static $flying = "yes";
+  public static $instance_count = 0;
+  public static $egg_num = 0;
 
-    function can_fly() {
-        if ( $this->flying == "yes" ) {
-            $flying_string = "can fly";
-        } else {
-            $flying_string = "is stuck on the ground";
-        }
-        return  $flying_string ;
-    }
+  public static function create()
+  {
+    $class_name = get_called_class();
+    $obj = new $class_name;
+    self::$instance_count++;
+    return $obj;
+  }
+
+  public static function can_fly()
+  {
+    $flying_string = (static::$flying == "yes") ? "can fly" : "is stuck on the ground";
+    return $flying_string;
+  }
+
 }
 
-class YellowBelliedFlyCatcher extends Bird {
-    var $name = "yellow-bellied flycatcher";
-    var $diet = "mostly insects.";
-    var $song = "flat chilk";
+class YellowBelliedFlyCatcher extends Bird
+{
+  var $name = "yellow-bellied flycatcher";
+  var $diet = "mostly insects.";
+  var $song = "flat chilk";
+  public static $egg_num = '3-4, sometimes 5';
 }
 
-class Kiwi extends Bird {
-    var $name = "kiwi";
-    var $diet = "omnivorous";
-    var $flying = "no";
+class Kiwi extends Bird
+{
+  var $name = "kiwi";
+  var $diet = "omnivorous";
+  public static $flying = "no";
 }
--- asgn03-static/index.php
+++ asgn03-static/index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>asgn02 Inheritance</title>
-</head>
-<body>
-<h1>Inheritance Examples</h1>
+  </head>
 
-<?php 
+  <body>
+    <h1>Inheritance Examples</h1>
+
+    <?php
     include 'Bird.php';
-    
+
     $bird = new Bird;
     echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
 
@@ -18,11 +20,31 @@
     echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
 
     $kiwi = new Kiwi;
-    $kiwi->flying = "no";
-    echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
-    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";    
+    Kiwi::$flying = "no";
 
-?>
-    </body>
+    echo "<p>The " . $fly_catcher->name . " " . YellowBelliedFlyCatcher::can_fly() . ".</p>";
+    echo "<p>The " . $kiwi->name . " " . Kiwi::can_fly() . ".</p>";
+
+    echo "<p>The " . $fly_catcher->name . " will lay  " . YellowBelliedFlyCatcher::$egg_num . " eggs.</p>";
+    echo "<p>The " . $kiwi->name . " will lay  " . Kiwi::$egg_num . " eggs.</p>";
+ 
+
+    echo "There are: " . Bird::$instance_count .  " Birds.<br/>";
+    echo "There are: " . YellowBelliedFlyCatcher::$instance_count . " Yellow Bellied Flycatchers.<br/>";
+    echo "There are: " . Kiwi::$instance_count . " Kiwis.<br/>";
+
+    echo "<hr/>";
+
+    $newBird = Bird::create();
+    $newFlyCatcher = YellowBelliedFlyCatcher::create();
+    $newKiwi = Kiwi::create();
+
+    echo "There are: " . Bird::$instance_count .  " Birds.<br/>";
+    echo "There are: " . YellowBelliedFlyCatcher::$instance_count . " Yellow Bellied Flycatchers.<br/>";
+    echo "There are: " . Kiwi::$instance_count . " Kiwis.<br/>";
+
+
+    ?>
+  </body>
+
 </html>
-
